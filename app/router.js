a962d779daf27cc2ca745fa7eb4408288cb2aaa2
5adiyah <sadiali@pdx.edu>
2016-06-08 23:26:24 -0700

    add more links

--- app/router.js
+++ app/router.js
@@ -8,6 +8,14 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('post');
   this.route('post', {path: '/post/:post_id'});
+  this.route('todo');
+  this.route('calender');
+  this.route('calendar');
+  this.route('notebook');
+  this.route('codesnip');
+  this.route('bookmarks');
+  this.route('about');
+  this.route('contact');
 });
 
 export default Router;
